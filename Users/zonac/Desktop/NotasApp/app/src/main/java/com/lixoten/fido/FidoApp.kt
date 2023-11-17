5f1a358cedcddf3da56596ef9d2f0f6841ac1152
Daniellop945 <s20120175@alumnos.itsur.edu.mx>
2023-11-16 23:54:30 -0600

    Avance movil

---
+++ Users/zonac/Desktop/NotasApp/app/src/main/java/com/lixoten/fido/FidoApp.kt
@@ -0,0 +1,21 @@
+package com.lixoten.fido
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.lixoten.fido.navigation.FidoNavHost
+
+@Preview
+@Composable
+fun FidoApp(
+    navController: NavHostController = rememberNavController()
+) {
+    FidoNavHost(navController = navController)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultAppPreview() {
+    FidoApp()
+}
